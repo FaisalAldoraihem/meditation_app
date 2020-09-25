e55a7a918b25e9959bf3eda8c7200b3633a36d9b
keeperofthescum <faisalaldoraihem@gmail.com>
2020-09-26 01:29:59 +0300

    Main setup
    Firebase auth setup and userRepo added

---
+++ lib/src/screens/mainScreens/intro_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class IntroScreen extends StatefulWidget {
+  @override
+  _IntroScreenState createState() => _IntroScreenState();
+}
+
+class _IntroScreenState extends State<IntroScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
